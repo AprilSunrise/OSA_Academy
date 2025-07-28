37e90c60144b14cb6fe8a5f1d561d409d26bc147
Audrey <Audreyhou2020@gmail.com>
2025-07-28 14:20:33 -0700

    Initial Commit

---
+++ OSA_Academy/OSA_AcademyApp.swift
@@ -0,0 +1,17 @@
+//
+//  OSA_AcademyApp.swift
+//  OSA_Academy
+//
+//  Created by Audrey on 7/28/25.
+//
+
+import SwiftUI
+
+@main
+struct OSA_AcademyApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ OSA_AcademyTests/OSA_AcademyTests.swift
@@ -0,0 +1,17 @@
+//
+//  OSA_AcademyTests.swift
+//  OSA_AcademyTests
+//
+//  Created by Audrey on 7/28/25.
+//
+
+import Testing
+@testable import OSA_Academy
+
+struct OSA_AcademyTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}

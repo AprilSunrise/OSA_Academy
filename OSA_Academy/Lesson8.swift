1ac8356a6b37cfaab002f8e3989b5a96a8655b30
Saanvi Krishnareddygari <saanvi.krishnareddygari@gmail.com>
2025-07-31 10:37:45 -0700

    lesson pages and home screen

---
+++ OSA_Academy/Lesson8.swift
@@ -0,0 +1,18 @@
+//
+//  Lesson8.swift
+//  OSA_Academy
+//
+//  Created by Saanvi Krishnareddygari on 7/31/25.
+//
+
+import SwiftUI
+
+struct Lesson8: View {
+    var body: some View {
+        Text("Lesson 8")
+    }
+}
+
+#Preview {
+    Lesson8()
+}

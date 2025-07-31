41c00e9d60658645eda331f7a45cdacb0c0fc687
Audrey <Audreyhou2020@gmail.com>
2025-07-31 10:41:20 -0700

    lesson 3

--- OSA_Academy/Lesson3.swift
+++ OSA_Academy/Lesson3.swift
@@ -9,7 +9,45 @@ import SwiftUI
 
 struct Lesson3: View {
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        VStack {
+            Text("Lesson 3: Comparison")
+                .font(.title)
+                .fontWeight(.bold)
+                .multilineTextAlignment(.center)
+            Text(" Operators:")
+                .font(.title)
+                .fontWeight(.bold)
+                .multilineTextAlignment(.center)
+                
+            Text("Comparison operators in Swift allow us to compare values. Each expression below is evaluated to a boolean (boo-lee-uhn), true or false. ")
+                .font(.body)
+                .multilineTextAlignment(.center)
+                .padding(.horizontal)
+                .padding(.vertical)
+            
+            Text("3 < 5 = //-> true. ")
+                .foregroundColor(.green)
+                .multilineTextAlignment(.center)
+                .padding()
+            Text("4 >= 9 //-> false")
+                .foregroundColor(.red)
+                .multilineTextAlignment(.center)
+                .padding()
+            Text("7 == 7 //-> true \nkarlie == karlie //-> true")
+                .lineSpacing(50)
+                .foregroundColor(.green)
+                .multilineTextAlignment(.center)
+                .padding()
+                .padding()
+            Text("karlie == Karlie //-> false")
+                .foregroundColor(.red)
+                .multilineTextAlignment(.center)
+
+
+
+            }
+            .padding()
+
     }
 }
 
